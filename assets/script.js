48f03878b2b755c0a8f10fc6fa07e02064eea50b
Stefan Marinov <stefan_marinov@rogers.com>
2023-01-03 18:47:04 -0500

    Added some JS code

---
+++ assets/script.js
@@ -0,0 +1,26 @@
+/*var topic = "css";
+
+if (topic==="HTML") {
+    console.log("Let's study HTML!")
+} else if(topic ==="CSS") {
+    console.log("Let's study CSS!")
+} else if (topic ==="GIT") {
+    console.log("Let's study GIT!")
+} else if (topic==="JavaScript") {
+    console.log("Let's study JAvaScript!")
+} else {
+    console.log("Please try again!")
+}
+
+var shapes = ["triangle", "square", "pentagon", "circle"];
+console.log(shapes[0]);
+
+for (var shape=0; shape<shapes.length; shape++) {
+    console.log(shapes[shape]);
+}*/
+
+var topics = ["HTML", "CSS", "GIT", "JavaScript"];
+
+for (var topic=0; topci<topics.length; topic++) {
+    console.log(topics[topic]);
+}
